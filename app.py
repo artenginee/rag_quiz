19cd9cf3e4abc8030f587c2ea0d6fd7c1a62932c
art engineer <taeeui.k@gmail.com>
2024-06-09 19:36:07 +0900

    sqlite version add

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from llm import generate_quiz
 
+import(‘pysqlite3’)
+import sys
+sys.modules[‘sqlite3’] = sys.modules.pop(‘pysqlite3’)
+
 st.set_page_config(
     page_title="QUIZ",
     page_icon="🧊",
